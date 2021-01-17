ed4c7e32e16822a907a798ca930e5902c734a290
JinilSon <dmstnr1320@naver.com>
2021-01-17 21:41:36 +0900

    DFS 음료수 얼리기 알고리즘

---
+++ DFS_Frozen_drink.py
@@ -0,0 +1,33 @@
+import time
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+start_time = time.time()
+
+
+def dfs(x, y):
+    if x <= -1 or x >=n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+end_time = time.time()
+print(result)
+print("경과시간 :", end_time-start_time)
