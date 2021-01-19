f5f7ef5d26919c9eaadc1c3507eb68eecfd3085b
JinilSon <dmstnr1320@naver.com>
2021-01-19 20:55:20 +0900

    DFS_아이스크림(주석처리)

--- DFS_Frozen_drink.py
+++ DFS_Frozen_drink.py
@@ -10,14 +10,14 @@ start_time = time.time()
 
 
 def dfs(x, y):
-    if x <= -1 or x >=n or y <= -1 or y >= m:
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     if graph[x][y] == 0:
-        graph[x][y] = 1
-        dfs(x - 1, y)
-        dfs(x, y - 1)
-        dfs(x+1, y)
-        dfs(x, y+1)
+        graph[x][y] = 1     # 현재 노드를 방문처리하고,,
+        dfs(x - 1, y)       # 상
+        dfs(x, y - 1)       # 우
+        dfs(x+1, y)         # 하
+        dfs(x, y+1)         # 좌 중에서 0인 값을 모두 방문처리한다.
         return True
     return False
 
@@ -25,7 +25,7 @@ def dfs(x, y):
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i, j) == True:
+        if dfs(i, j) == True:   # 한 덩어리의 아이스크림을 모두 방문처리했으면 result 값을 올린다
             result += 1
 
 end_time = time.time()
