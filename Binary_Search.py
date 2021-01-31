fc48f20081c02fa9cb1f4c7a5c4e490daa159aaa
JinilSon <dmstnr1320@naver.com>
2021-01-31 18:27:15 +0900

    이진탐색 알고리즘

--- Binary_Search.py
+++ Binary_Search.py
@@ -21,16 +21,37 @@
 
 
 # 이진탐색은 변수 3개(시작점, 끝점, 중간점)을 이용하여 반복적으로 비교하여 찾는 탐색과정이다.
+# def binary_search(array, target, start, end):
+#     if start > end:
+#         return None
+#     mid = (start + end) // 2
+#     if array[mid] == target:
+#         return mid
+#     elif array[mid] > target:
+#         return binary_search(array, target, start, mid - 1)
+#     else:
+#         return binary_search(array, target, mid + 1, end)
+#
+# n, target = list(map(int, input().split()))
+# array = list(map(int, input().split()))
+#
+# result = binary_search(array, target, 0, n - 1)
+# if result == None:
+#     print("해당하는 원소가 없습니다.")
+# else:
+#     print(result + 1)
+
+# 반복문을 이용한 이진탐색
+
 def binary_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    else:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
 
 n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
